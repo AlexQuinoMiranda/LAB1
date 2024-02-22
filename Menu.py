74369df72c25c5fb306be2d5b599276808171612
andy145235 <andyn7.10.11@gmail.com>
2024-02-22 02:17:44 -0400

    El menu principal del inicio

---
+++ Menu.py
@@ -0,0 +1,12 @@
+def mostrar_menu():
+    print("\nMenú:")
+    print("1. Agregar Tarea")
+    print("2. Marcar Tarea como Completada")
+    print("3. Eliminar Tarea")
+    print("4. Ver Tareas Pendientes")
+    print("5. Ver Tareas Completadas")
+    print("6. Guardar y Salir")
+    print("7. Salir sin Guardar")
+
+if __name__ == "__main__":
+    mostrar_menu()
